41216fca2b83aed754e10411cf23807687ded836
Juan Félix <fectec151@gmail.com>
2024-12-01 21:20:10 -0600

    Update jingle.h

--- CM7/Core/Inc/jingle.h
+++ CM7/Core/Inc/jingle.h
@@ -5,15 +5,18 @@
 #ifndef INC_JINGLE_H_
 #define INC_JINGLE_H_
 
-#define TIM_FREQ 240000000  // The frequency of the timer's clock
+// Extern variables
 
 extern TIM_HandleTypeDef htim1;
 
 extern int melody[];
 extern int durations[];
 extern int pause[];
-
 extern size_t melody_size;
+
+// Defines
+
+#define TIM_FREQ 240000000  // Frequency of the timer's clock
 
 // Function prototypes
 
