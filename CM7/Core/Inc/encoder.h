bfd5797c13c7da76386d4f9b87b10b2cb82c1e0c
Juan Félix <fectec151@gmail.com>
2024-12-01 21:19:31 -0600

    Update encoder.h

--- CM7/Core/Inc/encoder.h
+++ CM7/Core/Inc/encoder.h
@@ -7,9 +7,9 @@
 
 // Defines
 
-#define WHEEL_RADIUS	0.034     							             // Meters
-#define ENCODER_CPR		210
-#define WHEEL_CIRCUMFERENCE (2 * 3.14159 * WHEEL_RADIUS) 	 // Meters
+#define WHEEL_RADIUS 0.034                               // Meters
+#define ENCODER_CPR  210
+#define WHEEL_CIRCUMFERENCE (2 * 3.14159 * WHEEL_RADIUS) // Meters
 
 // Function prototypes
 
